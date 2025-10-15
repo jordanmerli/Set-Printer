5cad4b5af558b0e005c35a6632310143a6638cd3
Jordan Merli <jordanmerli90@gmail.com>
2025-10-15 13:56:44 +0200

    Set Printer

---
+++ SelezionaStampante/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ProvaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon1.BalloonTipText = "Clicca per selezionare la stampante predefinita"
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Clicca per selezionare la stampante predefinita"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.BackColor = System.Drawing.SystemColors.Control
+        Me.ContextMenuStrip1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProvaToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(153, 48)
+        '
+        'ProvaToolStripMenuItem
+        '
+        Me.ProvaToolStripMenuItem.Checked = True
+        Me.ProvaToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.ProvaToolStripMenuItem.Name = "ProvaToolStripMenuItem"
+        Me.ProvaToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ProvaToolStripMenuItem.Text = "Prova"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(254, 124)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Seleziona Stampante"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ProvaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ SelezionaStampante/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Drawing.Printing
+Public Class Form1
+    Dim strPrinterName As String
+    Private Sub ContextMenuStrip1_ItemClicked(ByVal sender As Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles ContextMenuStrip1.ItemClicked
+        'MsgBox(e.ClickedItem.Text)         'se tolgo l'apostrofo compare una messagebox con scritto l'elemento cliccato
+        If e.ClickedItem.Text.Trim <> "CHIUDI" Then     'se clicco qualcosa che non sia Chiudi
+            strPrinterName = e.ClickedItem.Text         'imposto la stampante selezionata
+            Call SetDefaultSystemPrinter(strPrinterName)   'e chiamo la funzione SetDefaultSystemPrinter(strPrinterName) che ho creato sotto
+        Else
+            Call EsciProgramma()            'altrimenti chiamo la funzione EsciProgramma
+        End If
+    End Sub
+    Private Sub ContextMenuStrip1_Opening(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+        ContextMenuStrip1.Items.Clear()
+        For Each strPrinterName As String In PrinterSettings.InstalledPrinters      'per ogni stampante installata
+            Dim itm As New ToolStripMenuItem 'ToolStripButton
+            itm.Text = strPrinterName
+            itm.DisplayStyle = ToolStripItemDisplayStyle.Text
+            ContextMenuStrip1.Items.Add(itm)
+            Dim PD As New PrintDocument, StampPred As String
+            StampPred = PD.DefaultPageSettings.PrinterSettings.PrinterName
+            If StampPred = itm.Text Then
+                itm.Checked = True
+                itm.CheckState = CheckState.Checked
+            End If
+        Next
+        Dim itm2 As New ToolStripMenuItem 'ToolStripButton
+        itm2.Text = ""
+        itm2.DisplayStyle = ToolStripItemDisplayStyle.Text
+        ContextMenuStrip1.Items.Add(itm2)
+        Dim itm3 As New ToolStripMenuItem 'ToolStripButton
+        itm3.Text = "CHIUDI"
+        itm3.DisplayStyle = ToolStripItemDisplayStyle.Text
+        ContextMenuStrip1.Items.Add(itm3.Text)
+    End Sub
+    Sub EsciProgramma()         'funzione esci programma
+        Dim jordan
+        jordan = MsgBox("Sei sicuro di voler uscire?", MsgBoxStyle.Information + MsgBoxStyle.YesNo, "Esci!")
+        If jordan = vbYes Then
+            End
+        End If
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Hide()           'all'avvio nascondo il form
+    End Sub
+    'Private Sub NotifyIcon1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.Click
+    'Dim Position As System.Drawing.Point
+    'Position.X = MousePosition.X
+    'Position.Y = MousePosition.Y
+    'ContextMenuStrip1.Show(Position.X, Position.Y)
+    'End Sub
+    '
+    'Private Sub NotifyIcon1_MouseDoubleClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+    'Dim Position As System.Drawing.Point
+    'Position.X = MousePosition.X
+    'Position.Y = MousePosition.Y
+    'ContextMenuStrip1.Show(Position.X, Position.Y)
+    'End Sub
+    Private Function SetDefaultSystemPrinter(ByVal strPrinterName As String) As Boolean         'funzione imposta stampante predefinita
+        ' Esempio: SetDefaultSystemPrinter("Lexmark T620") - local printer
+        ' Esempio: SetDefaultSystemPrinter("\\Server01\Lexmark T522") - network printer
+        Dim strOldPrinter As String
+        Dim WshNetwork As Object
+        Dim pd As New PrintDocument
+        ' Set the default system printer
+        Try
+            strOldPrinter = pd.PrinterSettings.PrinterName '/ Get the system default printer
+            WshNetwork = Microsoft.VisualBasic.CreateObject("WScript.Network")
+            WshNetwork.SetDefaultPrinter(strPrinterName)
+            pd.PrinterSettings.PrinterName = strPrinterName '/ Specify the printer to use.
+            If pd.PrinterSettings.IsValid Then '/ Check that the printer exists
+                Return True
+            Else
+                'MessageBox.Show("Printer <" & strPrinterName & "> is invalid.")
+                WshNetwork.SetDefaultPrinter(strOldPrinter)
+                Return False
+            End If
+        Catch exptd As Exception
+            WshNetwork.SetDefaultPrinter(strOldPrinter)
+            Return False
+        Finally
+            WshNetwork = Nothing
+            pd = Nothing
+        End Try
+    End Function '/ SetDefaultSystemPrinter
+End Class
